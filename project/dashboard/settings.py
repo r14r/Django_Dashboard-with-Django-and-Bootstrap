2ac3bd3a53505882e024c8ed162b0dcb7698ba7b
Ralph Göstenmeier <r14r@via-internet.de>
2023-08-26 18:11:58 +0200

    move up static folder

--- project/dashboard/settings.py
+++ project/dashboard/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dashboard/static')
+    os.path.join(BASE_DIR, 'dashboard.static')
 ]
 
 # Default primary key field type
